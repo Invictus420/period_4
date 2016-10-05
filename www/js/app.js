67fc0d34d83dd4de3e0ae4d1078c8700be619d35
Alexandar Kraunsøe <Alexandar Kraunse>
2016-10-05 18:18:34 +0200

    init

---
+++ www/js/app.js
@@ -0,0 +1,107 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+  .controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicModal, $http) {
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var showF = function(f){
+        f.forEach(function(friend, index, arr){
+          var loc = new google.maps.LatLng(friend.loc.coordinates[1], friend.loc.coordinates[0]);
+          var marker = new google.maps.Marker({
+            map: $scope.map,
+            animation: google.maps.Animation.DROP,
+            position: loc
+          });
+          var infoWindow = new google.maps.InfoWindow({
+            content: friend.userName
+          });
+
+          google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open($scope.map, marker);
+          });
+        })
+      }
+
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+        });
+        var infoWindow = new google.maps.InfoWindow({
+          content: "Here I am!"
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+        });
+      });
+
+      $scope.user = {};
+      $scope.registerUser = function (user) {
+        $scope.modal.hide();
+        console.log("1: " + user.userName);
+        console.log("2: " + user.distance);
+        user.loc = {type: "Point", coordinates: []}; //GeoJSON point
+        user.loc.coordinates.push(latLng.lng()); //Observe that longitude comes first
+        user.loc.coordinates.push(latLng.lat()); //in GEoJSON
+        console.log(JSON.stringify(user));
+        $http({
+          method: "POST",
+          url: " http://ionicboth-plaul.rhcloud.com/api/friends/register/"+user.distance,
+          data: user
+        }).then(function succes(res){
+          if(res.data!=null){
+            showF(res.data)
+          }
+        }) //Your task is to handle the response
+      }
+    }, function(error){
+      console.log("Could not get location");
+    });
+  });
